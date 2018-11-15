d052c4ac07cbe686f2648a2b42c0facbd09ebc7b
Mayan robotics <mayan.robotics@gmail.com>
2018-11-15 19:56:50 +0200

    Drrive by vuforia function
    Function that driver until the gold mineral is in font of the robot, by our webcam added.

--- AutoMain.java
+++ AutoMain.java
@@ -15,6 +15,8 @@ import org.opencv.core.Rect;
 import org.opencv.imgproc.Imgproc;
 import java.util.ArrayList;
 import java.util.List;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+
 
 /**
  * Apollo Autonomus.
@@ -71,18 +73,17 @@ public abstract class AutoMain extends LinearOpMode
     //The main function of the autonomous
     void apolloRun(boolean isCrater)
     {
-        vision.setShowCountours(true);
+        //vision.setShowCountours(true);
 
-        moveToGoldMineralByCamera();
-        if (moveToGoldMineralByCamera() == SuccessORFail.FAIL){
+        // Drive until the gold mineral is in front of the robot, by camera.
+        moveToGoldMineralByVuforia();
+        if (moveToGoldMineralByVuforia() == SuccessORFail.FAIL){
             telemetry.addData("Vision","FAILED");
             telemetry.update();
         } else{
             telemetry.addData("Vision","cube moved");
             telemetry.update();
         }
-
-
         if(isCrater)
         {
 
@@ -94,72 +95,86 @@ public abstract class AutoMain extends LinearOpMode
 
     }
 
-    //Function returns which mineral the camera detected.
-    public HardwareApollo.TYPE_MINERALS mineralTypeDetect()
-    {
-        vision.setShowCountours(true);
-        if (vision.goldMineralFound() == true) {
-            return HardwareApollo.TYPE_MINERALS.GOLD;
-        }
-        telemetry.update();
-        return null;
-    }
-
-    //Function returns the location of the gold mineral.
-    public GoldPosition GetGoldLocation()
-    {
-        contoursGold.clear();
-        vision.getGoldContours(contoursGold);
-
-        if ((vision.goldMineralFound()) && (contoursGold != null)) {
-            if (!contoursGold.isEmpty())  {
-                if (contoursGold.get(0) != null) {
-
-                    Rect GoldBoundingRect = Imgproc.boundingRect(contoursGold.get(0));
-                    int goldXPosition = GoldBoundingRect.x;
-
-                    if (goldXPosition < 450) {
-                        //telemetry.addData("Gold Position", "Left");
-                        return GoldPosition.LEFT;
-                    } else if (goldXPosition > 650) {
-                        //telemetry.addData("Gold Position", "Right");
-                        return GoldPosition.RIGHT;
-                    } else if (goldXPosition > 450 && goldXPosition < 650) {
-                        //telemetry.addData("Gold Position", "Middle");
-                        return GoldPosition.MIDDLE;
-                    }
-                }
-            }
-        }
-        return null;
-    }
-
-    public SuccessORFail moveToGoldMineralByCamera(){
-        if(GetGoldLocation()!= null) {
-            if (GetGoldLocation() == GoldPosition.LEFT) {
-                telemetry.addData("Drive", "left");
-                robot.setDriveMotorsPower(-DRIVE_SIDEWAYS_SPEED, HardwareApollo.DRIVE_MOTOR_TYPES.SIDE_WAYS);
-            } else if (GetGoldLocation() == GoldPosition.RIGHT) {
-                telemetry.addData("Drive", "right");
-                robot.setDriveMotorsPower(DRIVE_SIDEWAYS_SPEED, HardwareApollo.DRIVE_MOTOR_TYPES.SIDE_WAYS);            }
-            else if (GetGoldLocation() == GoldPosition.MIDDLE) {
-                telemetry.addData("Drive", "Middle");
-                robot.setDriveMotorsPower(0, HardwareApollo.DRIVE_MOTOR_TYPES.ALL);
-                return SuccessORFail.SUCCESS;
-            }else {
-                telemetry.addData("camera", "error");
-                return SuccessORFail.FAIL;
-            }
-        }
-        return null;
-    }
-
 
     public void waitSeconds(double seconds)
     {
         runtime.reset();
         while (opModeIsActive() && (runtime.seconds() < seconds)) {
         }
+    }
+
+    //Function drives until the gold mineral is in the middle by our camera.
+    public SuccessORFail moveToGoldMineralByVuforia(){
+        if(getVuforiaGoldMineralPosition()!= null) {
+            if (getVuforiaGoldMineralPosition() == GoldPosition.LEFT) {
+                telemetry.addData("Drive", "left");
+                robot.setDriveMotorsPower(-0.3, HardwareApollo.DRIVE_MOTOR_TYPES.SIDE_WAYS);
+            }
+            else if (getVuforiaGoldMineralPosition() == GoldPosition.RIGHT) {
+                telemetry.addData("Drive", "right");
+                robot.setDriveMotorsPower(0.3, HardwareApollo.DRIVE_MOTOR_TYPES.SIDE_WAYS);
+            }
+            else if (getVuforiaGoldMineralPosition() == GoldPosition.MIDDLE)
+            {
+                telemetry.addData("Drive", "Middle");
+                robot.setDriveMotorsPower(0, HardwareApollo.DRIVE_MOTOR_TYPES.ALL);
+                return SuccessORFail.SUCCESS;
+            }
+            else {
+                telemetry.addData("camera", "NO GOLD MINERAl");
+                return SuccessORFail.FAIL;
+            }
+        }
+        return null;
+    }
+
+    //Function returns the location of the gold mineral.
+    public GoldPosition getVuforiaGoldMineralPosition(){
+        if (opModeIsActive()) {
+
+            /** Activate Tensor Flow Object Detection. */
+            if (robot.tfod != null) {
+                robot.tfod.activate();
+            }
+        }
+        if (robot.tfod != null) {
+            if (opModeIsActive()) {
+                while (opModeIsActive()) {
+                    // getUpdatedRecognitions() will return null if no new information is available since
+                    // the last time that call was made.
+                    List<Recognition> updatedRecognitions = robot.tfod.getUpdatedRecognitions();
+                    if (updatedRecognitions != null) {
+                        telemetry.addData("# Object Detected", updatedRecognitions.size());
+                        if (updatedRecognitions.size() >= 1) {
+                            int goldMineralX;
+                            for (Recognition recognition : updatedRecognitions) {
+                                if (recognition.getLabel().equals(robot.LABEL_GOLD_MINERAL)) {
+                                    goldMineralX = (int) recognition.getLeft();
+                                    telemetry.addData("GoldX", goldMineralX);
+                                    telemetry.addData("# Object Detected2", updatedRecognitions.size());
+                                    telemetry.update();
+                                    if (goldMineralX < robot.MineralMiddleLimitLeft) {
+                                        telemetry.addData("Gold Position", "Left");
+                                        telemetry.update();
+                                        return GoldPosition.LEFT;
+                                    } else if (goldMineralX > robot.MineralMiddleLimitRight) {
+                                        telemetry.addData("Gold Position", "Right");
+                                        telemetry.update();
+                                        return GoldPosition.RIGHT;
+                                    } else if (goldMineralX > robot.MineralMiddleLimitLeft && goldMineralX < robot.MineralMiddleLimitRight) {
+                                        telemetry.addData("Gold Position", "Middle");
+                                        telemetry.update();
+                                        return GoldPosition.MIDDLE;
+                                    }
+                                }
+                            }
+                        }
+                        telemetry.update();
+                    }
+                }
+            }
+        }
+        return null;
     }
 
     /*
@@ -170,7 +185,6 @@ public abstract class AutoMain extends LinearOpMode
      *  2) Move runs out of time
      *  3) Driver stops the opmode running.
      */
-
     public void gyroDrive ( double speed,
                             double Distance,
                             double angle)
